372df4061506f2cb55c2abc420a7d15df156e98b
Stewino <sjombik@gmail.com>
2020-07-23 22:14:50 +0200

    first commit

---
+++ first_repo_python_app.py
@@ -0,0 +1,39 @@
+def main():
+    start_up_message()
+    input('Press enter to continue')
+    
+    step_1()
+    input('Press enter to continue')
+    
+    step_2()
+    input('Press enter to continue')
+    
+    step_3()
+    input('Press enter to continue')
+    
+    step_4()
+       
+
+def start_up_message():
+    print('This program tells you how to')
+    print('disassemble an ACME laundry dryer.')
+    print('There are 4 steps in the process.')
+    print()
+
+def step_1():
+    print('Step 1: Unplug the dryer and move it away from the wall.')
+    print()
+
+def step_2():
+    print('Step 2: Remove the six screws from the back of the dryer.')
+    print()
+
+def step_3():
+    print('Step 3: Remove the dryer’s back panel.')
+    print()
+
+def step_4():
+    print('Step 4: Pull the top of the dryer straight up.')
+    print()
+
+main()
